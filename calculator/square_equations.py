452d53d17fc60b1d39563dc9747b268fc798eadf
Mlotek1212 <mlotke1212@wp.pl>
2023-03-19 20:57:36 +0100

    add new file with square equations and tests

--- calculator/square_equations.py
+++ calculator/square_equations.py
@@ -32,4 +32,21 @@ def second_place_zero(
 
 
 def calculate_delta(a: int | float, b: int | float, c: int | float) -> int | float:
-    return subtraction_numbers(compounding_numbers(b, 2), multiplication_numbers(4, multiplication_numbers(a, c)))
+    result = subtraction_numbers(compounding_numbers(b, 2), multiplication_numbers(4, multiplication_numbers(a, c)))
+    if result < 0:
+        raise ValueError("delta can`t be less than zero")
+    return result
+
+
+def calculate_square_equations(a: int | float, b: int | float, c: int | float) -> tuple[int | float]:
+    try:
+        delta = calculate_delta(a, b, c)
+    except ValueError:
+        return tuple()
+    return tuple({first_place_zero(a, b, delta), second_place_zero(a, b, delta)})
+    # first = first_place_zero(a, b, delta)
+    # second = second_place_zero(a, b, delta)
+    # if first == second:
+    #     return first,
+    # return first, second
+
--- tests/test_calculator/test_square_equations.py
+++ tests/test_calculator/test_square_equations.py
@@ -1,4 +1,6 @@
-from calculator.square_equations import first_place_zero, second_place_zero, calculate_delta
+import pytest
+
+from calculator.square_equations import first_place_zero, second_place_zero, calculate_delta, calculate_square_equations
 
 
 def test_first_place_zero():
@@ -11,3 +13,22 @@ def test_second_place_zero():
 
 def test_calculate_delta():
     assert calculate_delta(1, 4, 3) == 4
+
+
+def test_calculate_delta_zero_place_zero():
+    with pytest.raises(ValueError) as exc:
+        calculate_delta(1, 1, 1)
+
+    assert "delta can`t be less than zero" in str(exc)
+
+
+def test_calculate_square_equations_one_place():
+    assert calculate_square_equations(1, 4, 4) == (-2,)
+
+
+def test_calculate_square_equations_two_place():
+    assert calculate_square_equations(-1, 8, -12) == (2, 6)
+
+
+def test_calculate_square_equations_zero_place():
+    assert calculate_square_equations(1, 4, 5) == tuple()
